666e6627cb6cc95585d340d6c7b7ed6d95a02f84
Matthias GALLIOU <matthias.galliou@gmail.com>
2022-05-20 16:43:56 +0200

    print blanks in with max width in print_tape

--- src/run_machine.ml
+++ src/run_machine.ml
@@ -41,18 +41,19 @@ let next_state tape index state_name machine =
     in
     (new_tape, new_index, transition)
 
-let print_tape tape i =
+let print_tape tape i blank =
+    let max_len = 20 in
     print_string (
         "[" ^ (if i = 0 then "" else String.slice tape 0 i)
         ^ "<" ^ String.slice tape i (i + 1) ^ ">" ^ String.slice tape (i + 1) 0
-    ^ ".........]")
+    ^ (String.make (max_len - String.length tape) (String.get blank 0)) ^ "]")
 
 let rec loop_machine (tape, index, transition, machine) =
     if List.mem machine.finals transition.to_state ~equal:(String.equal) then
         ()
     else
         let (new_tape, new_index, new_transition) = next_state tape index transition.to_state machine in
-        print_tape tape index;
+        print_tape tape index machine.blank;
         print_string (get_transition_string transition transition.to_state);
         loop_machine (new_tape, new_index, new_transition, machine)
 
