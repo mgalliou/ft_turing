2f5475eb36dbb8206bb787a33d0ea2eefd924072
Eliott D'HOMMEE <edhommee@e1r8p18.clusters.42paris.fr>
2022-05-23 15:14:37 +0200

    debug run_machine

--- src/run_machine.ml
+++ src/run_machine.ml
@@ -9,35 +9,35 @@ let err_bad_action = "Bad action"
 
 let get_state machine state_name =
     match List.find machine.transitions ~f:(
-            fun a -> String.equal a.name state_name
-        )
+        fun a -> String.equal a.name state_name
+    )
         with
         | Some state -> state
         | None -> raise (Bad_instruction (err_state_not_in_machine_definition, state_name))
 
-let get_transition (state : state) c =
-    match List.find state.transitions ~f:(
+let get_transition state_name c machine =
+    let this_state = get_state machine state_name in
+    match List.find this_state.transitions ~f:(
         fun a -> String.equal a.read (c |> Char.to_string)
-    )
+        )
     with
     | Some transition -> transition
-    | None -> raise (Bad_instruction (err_no_transition_in_state_with_read, (c |> Char.to_string) ^ " in " ^ state.name))
+    | None -> raise (Bad_instruction (err_no_transition_in_state_with_read, (c |> Char.to_string) ^ " in " ^ state_name))
 
-let next_state tape index state_name machine =
-    let state = get_state machine state_name in
-    let transition = get_transition state tape.[index] in
-    let new_tape = String.substr_replace_first ~pos:index tape ~pattern:transition.read ~with_:transition.write ^
-    if index = String.length tape - 1 && String.equal transition.action "RIGHT" then
-        machine.blank
+let write_to_tape index read write tape =
+    String.substr_replace_first ~pos:index tape ~pattern:read ~with_:write
+
+let longer_tape tape index blank =
+    if index = String.length tape -1 then
+        tape ^ blank
     else
-       ""
-    in
-    let new_index = match transition.action with
+        tape
+
+let get_new_index action index =
+    match action with
     | "RIGHT" -> index + 1
     | "LEFT" -> index - 1
-    | _ -> raise (Bad_instruction (err_bad_action ,  " in " ^ state.name))
-    in
-    (new_tape, new_index, transition)
+    | _ -> raise (Bad_instruction (err_bad_action ,  " in " ^ action))
 
 let print_tape tape i blank =
     let max_len = 20 in
@@ -46,14 +46,17 @@ let print_tape tape i blank =
         ^ "<" ^ String.slice tape i (i + 1) ^ ">" ^ String.slice tape (i + 1) 0
     ^ (String.make (max_len - String.length tape) (String.get blank 0)) ^ "]")
 
-let rec loop_machine (tape, index, transition, machine, state_name) =
-    if List.mem machine.finals transition.to_state ~equal:(String.equal) then
-        ()
-    else
-        let (new_tape, new_index, new_transition) = next_state tape index transition.to_state machine in
-        print_tape tape index machine.blank;
+let rec loop_machine (tape, index, machine, state_name) =
+    if not (List.mem machine.finals state_name ~equal:(String.equal)) then
+        let new_transition = get_transition state_name tape.[index] machine in
+        let new_index = get_new_index new_transition.action index in
+        let new_tape = write_to_tape index new_transition.read new_transition.write tape in
+        let new_tape = longer_tape new_tape index machine.blank in
+        print_tape tape index machine.blank ;
         print_string (get_transition_string new_transition state_name);
-        loop_machine (new_tape, new_index, new_transition, machine, new_transition.to_state)
+        loop_machine (new_tape, new_index, machine, new_transition.to_state)
+    else
+        print_tape tape index machine.blank
 
 let check_tape tape alphabet =
     if not (String.for_all tape ~f:(fun c ->  List.mem alphabet (c |> Char.to_string) ~equal:(String.equal))) then
@@ -61,12 +64,8 @@ let check_tape tape alphabet =
     else
         true
 
-let run_machine machine tape =
+let run_machine (machine: machine) tape =
     if check_tape tape machine.alphabet && check_machine machine then
-        let transition = get_transition (get_state machine machine.initial) tape.[0] in
         print_string (get_machine_string machine);
         print_string "********************************************************\n";
-        loop_machine (tape, 0, transition, machine, machine.initial)
-    else
-        ()
-
+        loop_machine (tape, 0, machine, machine.initial)
