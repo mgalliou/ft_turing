b704b3ac533586467f773e92acf8e4b539fa99e5
eliottdhommee <eliottdhommee@gmail.com>
2022-05-24 13:32:49 +0200

    add write_json to write json from machine

---
+++ src/write_json.ml
@@ -0,0 +1,72 @@
+open Core
+open Yojson
+open Read_json
+open Types
+
+let create_json machine =
+    let build_transition transition =
+        let open Yojson.Basic.Util in
+        `Assoc [
+            ("read", `String transition.read);
+            ("to_state", `String transition.to_state);
+            ("write", `String transition.write);
+            ("action", `String transition.action);
+        ]
+    in
+
+    let build_state (state : state) =
+        let open Yojson.Basic.Util in
+        (state.name, `List (List.map state.transitions ~f:build_transition))
+    in
+
+    let json_output =
+        let open Yojson.Basic.Util in
+  `Assoc
+    [
+      ("name", `String machine.name);
+      ("alphabet", `List (List.map machine.alphabet ~f:(fun a-> `String a)));
+      ("blank", `String machine.blank);
+      ( "states", `List (List.map machine.states_names ~f:(fun a-> `String a)));
+      ("initial", `String machine.initial);
+      ("finals", `List (List.map machine.finals ~f:(fun a-> `String a)));
+      ("transitions", `Assoc (List.map machine.transitions ~f:build_state))
+    ]
+    in
+    json_output
+
+let get_args () =
+    let usage_msg = "usage: ft_turing [-h] jsonfile input" in
+    let input_strs = ref [] in
+    let help = ref false in
+    let speclist = [
+        ("jsonfile", Arg.Set (ref false) , "\t\tjson description of the machine\n");
+            ("input", Arg.Set (ref false), "\t\tinput of the machine\n");
+            ("-h, --help", Arg.Set (ref false), "\t\tshow this help message and exit\n");
+            ("-h", Arg.Set help, "");
+            ("--help", Arg.Set help, "");
+            ("-help", Arg.Set (ref false), "")
+    ] in
+    let anon_fun filename = input_strs := filename :: !input_strs in
+    let () = Arg.parse speclist anon_fun usage_msg in
+    if !help then
+        raise (Arg.Help (Arg.usage_string speclist usage_msg));
+    List.nth !input_strs 0
+
+let main () =
+    try
+        let machine_file = get_args () in
+        (*let valid_arg = check_args(machine_file, tape) in*)
+        let machine = read_json(Option.value machine_file ~default:"") in
+        let json = create_json machine in
+        let oc = stdout in
+        Yojson.Basic.pretty_to_channel oc json;
+        output_string oc "\n"
+    with
+    | Arg.Help e -> print_endline e
+    | Invalid_machine e -> print_endline e
+    | Invalid_machine_state (e, v) -> printf "%s : %s" e v
+    | Bad_instruction (msg, error) -> print_endline (msg ^ error)
+    | Invalid_json e -> print_endline e
+
+let () =
+    main ();
